02af35d0dd4fec3ebecd0ae40d8e229e2952298a
Code-Name-Ankit <161154290+code-Name-ankit@users.noreply.github.com>
2025-12-19 12:19:22 +0530

    add url

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:4000"
+export const BASE_URL = "https://linkedin-3-4ms6.onrender.com"
 
 export const clientServer = axios.create({
   baseURL: BASE_URL,
